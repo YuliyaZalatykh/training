1b4696463123108be34eaea1450158dc2e2d79eb
Yuliya Zalatykh <yuliya_zalatykh@outlook.com>
2020-03-28 18:28:10 +0300

    feat: complete main task5 2nd way

---
+++ src/main/java/com/zalatykh/javaFundamentals/mainTask/Task5_2.java
@@ -0,0 +1,33 @@
+package com.zalatykh.javaFundamentals.mainTask;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Task5_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of month");
+        int number = scanner.nextInt();
+
+        Map<Integer, String> months = new HashMap<>();
+        months.put(1, "January");
+        months.put(2, "February");
+        months.put(3, "March");
+        months.put(4, "April");
+        months.put(5, "May");
+        months.put(6, "June");
+        months.put(7, "July");
+        months.put(8, "August");
+        months.put(9, "September");
+        months.put(10, "October");
+        months.put(11, "November");
+        months.put(12, "December");
+
+        if (number >= 1 && number <= 12) {
+            System.out.println(months.get(number));
+        } else {
+            System.out.println("Incorrect number of month");
+        }
+    }
+}
